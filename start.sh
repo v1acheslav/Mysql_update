36dadeceeae79c92097732f29877e2ec5770ab75
v1acheslav <47541816+v1acheslav@users.noreply.github.com>
2019-07-13 22:59:41 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 for ctid in `vzlist -1 | awk '{print $1}'`; do
-  if vzctl exec2 $ctid mysql --version | grep -q 'Ver 14.14 Distrib 5.5.'; 
-  then echo "Processing: "$ctid
+  if vzctl exec2 $ctid mysql --version | grep -q 'Ver 14.14 Distrib 5.5.'; then
+     echo "Processing: "$ctid
   cat <<EOF > /vservers/private/$ctid/etc/apt/sources.list.d/mariadb.list
       deb http://mirror.klaus-uwe.me/mariadb/repo/10.0/debian jessie main
 EOF
